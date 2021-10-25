f278299b3ac2a39c13627211d4e7cc55abc8d236
Kylian <kylian.paulin@epitech.eu>
2021-10-26 00:39:50 +0200

    Project Init

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View vertical>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
