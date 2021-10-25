37277d1a9868b22bf82b6feb94cd4e38298bdea3
Kylian <kylian.paulin@epitech.eu>
2021-10-26 01:16:55 +0200

    add: Login Microsoft365 with WebView

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View vertical>
-      <Text>Hello</Text>
-    </View>
+      <WebView
+        source={{ uri: 'https://login.microsoftonline.com/' }}
+        style={styles.webView}
+      />
   );
 }
 
 const styles = StyleSheet.create({
+  webView: {
+    marginTop: 20
+  }
 });
