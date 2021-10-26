569333ababbcf7d0c6c601438fec13927cf4eb2a
Kylian <kylian.paulin@epitech.eu>
2021-10-26 11:26:28 +0200

    add: Navigation Options

--- App.js
+++ App.js
@@ -1,18 +1,62 @@
-import React, { useState } from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const [canGoBack, setCanGoBack] = useState(false)
+  const [canGoForward, setCanGoForward] = useState(false)
+  const [currentUrl, setCurrentUrl] = useState('')
+
+  const webviewRef = useRef(null)
+
+  backButtonHandler = () => {
+    if (webviewRef.current)
+      webviewRef.current.goBack()
+  }
+  frontButtonHandler = () => {
+    if (webviewRef.current)
+      webviewRef.current.goForward()
+  }
+
   return (
-      <WebView
-        source={{ uri: 'https://login.microsoftonline.com/' }}
-        style={styles.webView}
-      />
+    <View style={{ flex: 1 }}>
+    <WebView
+      source={{ uri: 'https://login.microsoft.com/' }}
+      style={{ marginTop: 40 }}
+      renderLoading={() => (
+        <ActivityIndicator
+          color='#2ed2d2'
+          size='large'
+          style={{
+            flex: 1
+          }}
+        />
+      )}
+      startInLoadingState={true}
+      ref={webviewRef}
+      onNavigationStateChange={navState => {
+        setCanGoBack(navState.canGoBack)
+        setCanGoForward(navState.canGoForward)
+        setCurrentUrl(navState.url)
+      }}
+    />
+    <View
+      style={{
+        padding: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: '#2ed2d2'
+      }}>
+      <TouchableOpacity onPress={backButtonHandler}>
+        <Text style={{ color: 'white', fontSize: 24 }}>Back</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={frontButtonHandler}>
+        <Text style={{ color: 'white', fontSize: 24 }}>Forward</Text>
+      </TouchableOpacity>
+     </View>
+</View>
   );
 }
 
 const styles = StyleSheet.create({
-  webView: {
-    marginTop: 20
-  }
 });
